834f74f7b4cf98f3f4c7f5dd7d83594fdc95a472
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-01 18:30:12 +0500

    change users crud: get_user_by_id and etc

--- src/dto/users/crud.py
+++ src/dto/users/crud.py
@@ -1,30 +1,37 @@
-from typing import Annotated
-
 from fastapi import APIRouter, HTTPException, status, Depends
-from fastapi.responses import ORJSONResponse
-from pydantic import ValidationError, Field
+from sqlalchemy.exc import IntegrityError
 
-from src.auth.basic_auth import check_token, oauth2_scheme
 from src.dao.models import Password, User
 from src.database.database import CommonAsyncSession
-from src.dto.users.schemas import UserOutSchema, UserCreateSchema, UserNotFound
-from src.dto.users.utils import fetch_all_users, fetch_user_by_id
+from src.dto.users.schemas import (
+    UserOutSchema,
+    UserCreateSchema,
+    ErrorDetailSchema,
+    DeleteConfirmSchema,
+    UserUpdateSchema,
+)
+from src.dto.users.utils import (
+    fetch_all_users,
+    fetch_user_by_id,
+    get_current_active_user,
+)
 from src.dto.passwords.utils import create_hashed_password
 
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"],
+    dependencies=[Depends(get_current_active_user)],
 )
 
 
 @router.get(
-    "/",
+    "",
     response_model=list[UserOutSchema],
     responses={
         404: {
             "description": "Users not found",
-            "model": UserNotFound,
+            "model": ErrorDetailSchema,
         },
     },
 )
@@ -43,47 +50,75 @@ async def get_all_users(
     return users_orm
 
 
-@router.post("/", response_model=UserOutSchema, status_code=201)
+@router.get("/{user_id}", response_model=UserOutSchema)
+async def get_user_by_id(
+    session: CommonAsyncSession,
+    user_id: int,
+):
+    """Get user by id from database"""
+
+    user = await fetch_user_by_id(session, user_id)
+    return user
+
+
+@router.post(
+    "",
+    response_model=UserOutSchema,
+    status_code=201,
+    responses={
+        409: {
+            "description": "Error: Conflict",
+            "model": ErrorDetailSchema,
+        },
+    },
+)
 async def add_new_user(
     session: CommonAsyncSession,
     user: UserCreateSchema,
 ):
     """Add new user to database"""
-    async with session.begin():
-        new_password_orm: Password = await create_hashed_password(
-            session,
-            user.password,
+    try:
+        async with session.begin():
+            new_password_orm: Password = await create_hashed_password(
+                session,
+                user.password,
+            )
+            session.add(new_password_orm)
+            await session.flush()
+            new_user_orm = User(**user.model_dump(exclude={"password"}))
+            new_user_orm.password_id = new_password_orm.id
+            session.add(new_user_orm)
+    except IntegrityError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"{exc.orig.args.__str__()}",
         )
-        session.add(new_password_orm)
-        await session.flush()
-        new_user_orm = User(**user.model_dump(exclude={"password"}))
-        new_user_orm.password_id = new_password_orm.id
-        session.add(new_user_orm)
 
     return new_user_orm
 
 
-@router.delete("/{user_id}", response_class=ORJSONResponse)
+@router.patch("/{user_id}", response_model=UserOutSchema)
+async def update_partial_user(
+    session: CommonAsyncSession,
+    user_id: int,
+    updated_user: UserUpdateSchema,
+):
+    user = await fetch_user_by_id(session, user_id)
+    for name, value in updated_user.model_dump(exclude_unset=True).items():
+        setattr(user, name, value)
+    await session.commit()
+    return user
+
+
+@router.delete("/{user_id}", response_model=DeleteConfirmSchema)
 async def delete_user(
     session: CommonAsyncSession,
     user_id: int,
 ):
     """Delete user from database"""
+
     user = await fetch_user_by_id(session, user_id)
     await session.delete(user)
+    await session.commit()
 
     return {"deleted": "True"}
-
-
-@router.post("/{user_id}", response_model=UserOutSchema)
-async def get_user_by_password(
-    session: CommonAsyncSession,
-    user_id: int,
-    token: Annotated[str, Depends(oauth2_scheme)],
-):
-    """Delete user from database"""
-    print(f"{token=}")
-
-    user = await fetch_user_by_id(session, user_id)
-
-    return user
