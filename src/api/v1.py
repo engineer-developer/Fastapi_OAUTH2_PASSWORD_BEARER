301ef4243fd2aba6405235ea5921a5ad63613623
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-01 18:22:07 +0500

    add func get_api_version

--- src/api/v1.py
+++ src/api/v1.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import ORJSONResponse
 
 from src.dto.users.crud import router as users_router
 
@@ -6,3 +7,8 @@ router = APIRouter(prefix="/api")
 
 
 router.include_router(users_router)
+
+
+@router.get("", response_class=ORJSONResponse, tags=["Api"])
+async def get_api_version():
+    return {"api_version": "v1"}
