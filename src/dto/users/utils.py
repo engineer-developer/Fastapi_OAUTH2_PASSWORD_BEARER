4918dbf8eecefd47fae89c9aa4cb093ff0f6fa1d
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-01 20:14:59 +0500

    update users utils

--- src/dto/users/utils.py
+++ src/dto/users/utils.py
@@ -1,11 +1,10 @@
 from typing import Sequence, Annotated, Optional
 
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 
-from src.auth.oauth2_password_bearer.schemas import oauth2_scheme
+from src.auth.schemas import oauth2_scheme
 from src.dao.models import User
 from src.database.database import CommonAsyncScopedSession
 
@@ -16,37 +15,37 @@ async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
     stmt = select(User).order_by(User.id)
     result = await session.execute(stmt)
     users = result.scalars().all()
-
     return users
 
 
-async def fetch_user_by_id(session: AsyncSession, id: int) -> Optional[User]:
+async def fetch_user_by_id(
+    session: AsyncSession,
+    id: int,
+) -> Optional[User]:
     """Fetch user by id from database"""
 
     stmt = select(User).where(User.id == id)
     result = await session.execute(stmt)
     user = result.scalar_one_or_none()
-
     return user
 
 
 async def fetch_user_by_email(
     session: AsyncSession,
     email: str,
-) -> User:
+) -> Optional[User]:
     """Fetch user by email from database"""
 
     stmt = select(User).where(User.email == email)
     result = await session.execute(stmt)
     user = result.scalar_one_or_none()
-
     return user
 
 
 async def get_current_user(
     session: CommonAsyncScopedSession,
     token: Annotated[str, Depends(oauth2_scheme)],
-):
+) -> Optional[User]:
     """Get current login user"""
 
     user = await fetch_user_by_email(session, token)
@@ -67,5 +66,4 @@ async def get_current_active_user(
 
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
-
     return current_user
