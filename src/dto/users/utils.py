ca1a8793c771a8b71197edd90beec3e0de235a6f
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-01 18:36:22 +0500

    add funcs: fetch_user_by_email, get_current_user, get_current_active_user

--- src/dto/users/utils.py
+++ src/dto/users/utils.py
@@ -1,15 +1,19 @@
-from typing import Sequence
+from typing import Sequence, Annotated
 
+from fastapi import Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
-from src.dao.models import User
+from auth.oauth2_password_bearer import oauth2_scheme
+from dao.models import User
+from database.database import CommonAsyncScopedSession
 
 
 async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
-    """Fetch all users"""
+    """Fetch all users from database"""
 
-    stmt = select(User)
+    stmt = select(User).order_by(User.id)
     result = await session.execute(stmt)
     users = result.scalars().all()
 
@@ -17,10 +21,51 @@ async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
 
 
 async def fetch_user_by_id(session: AsyncSession, id: int) -> User:
-    """Fetch user by id"""
+    """Fetch user by id from database"""
 
     stmt = select(User).where(User.id == id)
     result = await session.execute(stmt)
     user = result.scalar_one_or_none()
 
     return user
+
+
+async def fetch_user_by_email(
+    session: AsyncSession,
+    email: str,
+) -> User:
+    """Fetch user by email from database"""
+
+    stmt = select(User).where(User.email == email)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    return user
+
+
+async def get_current_user(
+    session: CommonAsyncScopedSession,
+    token: Annotated[str, Depends(oauth2_scheme)],
+):
+    """Get current login user"""
+
+    user = await fetch_user_by_email(session, token)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    """Get current active login user"""
+
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    return current_user
