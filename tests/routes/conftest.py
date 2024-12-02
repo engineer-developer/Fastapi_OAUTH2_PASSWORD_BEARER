75ee6980b63864571af032637f1ac4adf52b629e
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-02 23:17:34 +0500

    add login with admin credentials

--- tests/routes/conftest.py
+++ tests/routes/conftest.py
@@ -1,11 +1,31 @@
 import pytest
+from httpx import AsyncClient
+
+
+@pytest.fixture
+async def auth_data():
+    return {
+        "grant_type": "password",
+        "username": "user@example.com",
+        "password": "1234567890",
+    }
+
+
+@pytest.fixture
+async def login(async_client: AsyncClient, auth_data):
+    auth_url = "/token"
+    auth_headers = {"Content-type": "application/x-www-form-urlencoded"}
+    auth_response = await async_client.post(
+        auth_url, data=auth_data, headers=auth_headers
+    )
+    assert auth_response.status_code == 200
 
 
 @pytest.fixture
 async def new_user():
-    return {"name": "Dart Weider"}
-
-
-@pytest.fixture
-async def new_tweet():
-    return {"tweet_data": "New message", "tweet_media_ids": [1, 2]}
+    return {
+        "username": "Bob",
+        "email": "bob@example.com",
+        "role": "client",
+        "password": "0987654321",
+    }
