0ef82c51e490b206fb9ca711b29a7ac112574f79
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-02 20:53:14 +0500

    auto adding to database admin user and him password

--- src/alembic/versions/2024_12_01_0406-9ea8d1aeeb1f_init_revision.py
+++ src/alembic/versions/2024_12_01_0406-9ea8d1aeeb1f_init_revision.py
@@ -10,6 +10,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import TableClause
 
 
 # revision identifiers, used by Alembic.
@@ -20,14 +21,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    passwords_table = op.create_table(
         "passwords",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("hashed_password", sa.String(), nullable=False),
         sa.Column("salt", sa.String(), nullable=False),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_passwords")),
     )
-    op.create_table(
+    users_table = op.create_table(
         "users",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("username", sa.String(), nullable=True),
@@ -49,6 +50,23 @@ def upgrade() -> None:
         sa.UniqueConstraint("email", name=op.f("uq_users_email_")),
         sa.UniqueConstraint("password_id", name=op.f("uq_users_password_id_")),
     )
+
+    def insert_value_in_table(table: TableClause, value: dict):
+        op.bulk_insert(table, [value, ])
+
+    password_value = {
+        "hashed_password": "bf91848c83fd4bd1928ed18083b441064e7918a4275c6aaf7f9865ef4c7bde2c67692b5a4f207abe0820fc1c91a3824938d1d265e9d3943ac89a9e9abe923327",
+        "salt": "ecb91d1e2b644bf3812ec7de603c10e5"
+    }
+    user_value = {
+        "username": "some_user",
+        "email": "user@example.com",
+        "role": "admin",
+        "is_active": True,
+        "password_id": 1
+    }
+    insert_value_in_table(passwords_table, password_value)
+    insert_value_in_table(users_table, user_value)
 
 
 def downgrade() -> None:
